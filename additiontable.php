74695f0828134310e451fce01b337699f2f1ec78
Samson Radje <samody2006@yahoo.com>
2021-12-22 11:51:41 +0100

    addin AY

--- additiontable.php
+++ additiontable.php
@@ -2,19 +2,17 @@
 <html>
 <head>
     <title>
-    + Table
+    Addition Table
     </title>
 </head>
 <body>
 
-<h1> + Table </h1>
+<h1> Addition Table </h1>
 <table align="left" border="1" cellpadding="3" cellspacing="3">
     <?php
-    for($tablenum=1; $tablenum<=15; $tablenum++)
-    {
+    for ($tablenum=1; $tablenum<=15; $tablenum++) {
         echo "<tr>";
-        for ($value=1; $value<=12; $value++)
-        {
+        for ($value=1; $value<=12; $value++) {
             echo "<td>$tablenum + $value = ".$tablenum + $value."</td>";
         }
         echo "</tr>";
